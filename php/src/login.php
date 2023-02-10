b1b7fb5fbb55cbbb3f5cd8516be025c7a6f6a2c7
Pyyxel <ax.vassaux@gmail.com>
2023-02-10 14:58:01 +0100

    ajout formulaire de conexion basique

---
+++ php/src/login.php
@@ -0,0 +1,26 @@
+<?php
+     session_start();
+     if(isset($_SESSION['connecte'])){
+        if($_SESSION['connecte'] == TRUE){
+            header('Location: /traitement/login.php');
+        }
+    }   
+?>  
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="traitement/login.php" method="post">
+        <label>Entrez l'email</label>
+        <input type="email" name="email" id="">
+        <label>Entrez le mot de passe</label>
+        <input type="text" name="mot_de_passe" id="">
+        <input type="submit" value="Se connecter">
+    </form>
+</body>
+</html>
---
+++ php/src/traitement/login.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    //donnée qui en pratique arriveront de la base de donnée mais pour l'exercise on les enregistrent dans 2 variable brut.
+    $email = 'email@email.fr';
+    $mot_de_passe = 'test';
+    if(isset($_SESSION['connecte'])){  
+        if($_SESSION['connecte'] == TRUE){
+            echo 'vous êtes connecté';
+            echo "<a href='logout.php'>Se déconnecter</a>";         
+        }
+    }else{
+        if($_POST['email'] == $email AND $_POST['mot_de_passe']){
+            echo 'vous êtes connecté';
+            $_SESSION['connecte'] = TRUE;
+            echo "<a href='logout.php'>Se déconnecter</a>";
+        }else{
+            echo 'les identifiants sont incorects';
+        }
+    }
+
+?>
